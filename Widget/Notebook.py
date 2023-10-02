f9b81ba1b616ffeba82973a3bfc0172934ac8156
Wiji <wiji.prabowo0857@gmail.com>
2023-10-02 12:28:21 +0700

    echo "# Learn_Tkinter" >> Menambahkan beberapa file nootebook,progressbar1,progressbar2, testing, treeview 1, treeview to display git init git add Scrollbar.py, Slider.py git commit -m "tambah, Radio Button.py, testing Menu.py, PanedWindow.py, MenuButton.py" git branch -M main git remote add origin https://github.com/Wijip/Learn_Tkinter.git git push -u origin main

---
+++ Widget/Notebook.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry('400x300')
+
+notebook = ttk.Notebook(root)
+notebook.pack(pady=10, expand=True)
+
+frame1 = ttk.Frame(notebook, width=400, height=280)
+frame2 = ttk.Frame(notebook, width=400, height=280)
+
+frame1.pack(fill='both', expand=True)
+frame2.pack(fill='both', expand=True)
+
+notebook.add(frame1, text='General Information')
+notebook.add(frame2, text='Profile')
+
+root.mainloop()
